ad7f66e26eda54cfe053ad3cedf99ae88e868dfe
trandinhnguyenqh-ui <trandinhnguyenqh@gmail.com>
2026-09-09 09:34:19 +0700

    Update version to 1.0.2

--- api/version.js
+++ api/version.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.status(200).json({
-    Version: "1.0.1",
+    Version: "1.0.2",
     Url2024: "https://github.com/trandinhnguyenqh-ui/ttfl-license-api/raw/refs/heads/main/menu%20pccc.dll",
     Url2025: "https://github.com/trandinhnguyenqh-ui/ttfl-license-api/raw/refs/heads/main/menu%20pccc.dll",
     ReleaseNotes: "Cập nhật tính năng Trợ lý AI và Fix lỗi UI."
